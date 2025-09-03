1617fd961801cb842ec4b83f7810c33c5170ea09
kolamounika064-maker <kolamounika064@gmail.com>
2025-09-03 23:08:30 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,4 +2,4 @@
 echo "========== AFTER INSTALL =========="
 echo "Setting permissions and preparing environment..."
 sudo chown -R ec2-user:ec2-user /var/www/html
-sudo cp -r /home/ec2-user/src/index.html /var/www/html/
+sudo cp -r /home/ec2-user/rnd-app/src/index.html /var/www/html/
